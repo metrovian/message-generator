fc8f2db2223b181244da3113e74b3ae55d9044f4
Vian Lee <paramedoct@gmail.com>
2024-10-19 22:28:20 +0900

    UART 연결 구성

--- SerialCommunication/UART.cpp
+++ SerialCommunication/UART.cpp
@@ -1,5 +1,81 @@
 #include "UART.h"
 
+bool UART::open()
+{
+    if (com != NULL) return false;
+
+    com = CreateFileA
+    (
+        port.c_str(), 
+        GENERIC_READ | GENERIC_WRITE,
+        0,
+        NULL,
+        OPEN_EXISTING,
+        0,
+        NULL
+    );
+
+    if (com == INVALID_HANDLE_VALUE) 
+    {
+        std::cerr << "[" << port << "] Open Failed" << std::endl;
+        return false;
+    }
+
+    DCB dcbs = { 0 };
+    dcbs.DCBlength = sizeof(dcbs);
+
+    if (!GetCommState(com, &dcbs)) 
+    {
+        std::cerr << "[" << port << "] Status Check Failed" << std::endl;
+        close();
+
+        return false;
+    }
+
+    dcbs.BaudRate = baud;
+    dcbs.ByteSize = data;
+    dcbs.Parity = par;
+    dcbs.StopBits = stop;
+
+    if (!SetCommState(com, &dcbs)) 
+    {
+        std::cerr << "[" << port << "] Frame Configuration Failed" << std::endl;
+        close();
+
+        return false;
+    }
+
+    COMMTIMEOUTS timeouts = { 0 };
+    timeouts.ReadIntervalTimeout = 50;
+    timeouts.ReadTotalTimeoutConstant = 50;
+    timeouts.ReadTotalTimeoutMultiplier = 10;
+    timeouts.WriteTotalTimeoutConstant = 50;
+    timeouts.WriteTotalTimeoutMultiplier = 10;
+
+    if (!SetCommTimeouts(com, &timeouts)) 
+    {
+        std::cerr << "[" << port << "] Timeout Configuration Failed" << std::endl;
+        close();
+
+        return false;
+    }
+
+    return true;
+}
+
+bool UART::close()
+{
+    if (com != NULL) 
+    {
+        CloseHandle(com);
+        com = NULL;
+
+        return true;
+    }
+
+    return false;
+}
+
 std::vector<std::string> UART::searchPorts()
 {
     std::vector<std::string> ret;
--- SerialCommunication/UART.h
+++ SerialCommunication/UART.h
@@ -6,6 +6,20 @@
 
 class UART
 {
-public:
+protected: /* handle */
+    HANDLE com = NULL;
+
+protected: /* config */
+    std::string port = "COM5";
+    DWORD baud = 9600;
+    BYTE data = 8;
+    BYTE par = 0;
+    BYTE stop = 0;
+
+public: /* connection */
+    bool open();
+    bool close();
+
+public: /* search */
     static std::vector<std::string> searchPorts();
 };
