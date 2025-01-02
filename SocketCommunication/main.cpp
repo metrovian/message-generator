f902ac923d2cee23d3781b14bcbdacb1c9c52616
Vian Lee <paramedoct@gmail.com>
2025-01-02 22:03:46 +0900

    HTTP Server POST 작업내용 백업

--- SocketCommunication/main.cpp
+++ SocketCommunication/main.cpp
@@ -6,11 +6,6 @@ int main()
 	ServerHTTP http;
 	http.open();
 	getchar();
-	ClientHTTP cc;
-	cc.connect("127.0.0.1");
-	getchar();
-	cc.sendRequestMessage({ "GET", "/", "HTTP/1.1" });
-	getchar();
 	http.close();
 	getchar();
 	return 0;
