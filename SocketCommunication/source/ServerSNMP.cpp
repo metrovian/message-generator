30edd670174357f3521faea41660bb28a014e1be
metrovian <paramedoct@gmail.com>
2025-02-26 23:53:35 +0900

    SNMP Request 작업내용 백업

--- SocketCommunication/source/ServerSNMP.cpp
+++ SocketCommunication/source/ServerSNMP.cpp
@@ -12,6 +12,92 @@ ServerSNMP::~ServerSNMP()
 	stopThread(162);
 }
 
+std::string ServerSNMP::encodeNumberASN1(int32_t _num)
+{
+	std::string ret;
+
+	ret.push_back(0x02);
+	ret.push_back(0x04);
+
+	ret.push_back(static_cast<char>(0xFF & (_num >> 24)));
+	ret.push_back(static_cast<char>(0xFF & (_num >> 16)));
+	ret.push_back(static_cast<char>(0xFF & (_num >> 8)));
+	ret.push_back(static_cast<char>(0xFF & (_num >> 0)));
+
+	return ret;
+}
+
+std::string ServerSNMP::encodeMessageASN1(std::string _str)
+{
+	std::string ret;
+
+	ret.push_back(0x04);
+	ret.push_back(_str.size());
+
+	ret += _str;
+
+	return ret;
+}
+
+std::string ServerSNMP::encodeFrameASN1(SNMP_SESSION _ses, SNMP_FRAME _msg)
+{
+	std::string ret;
+
+	uint64_t sofs;
+	uint64_t bofs;
+	uint64_t cofs;
+
+	ret += encodeNumberASN1(_msg.version);
+	ret += encodeMessageASN1(_msg.community);
+
+	ret.push_back(static_cast<char>(_ses));
+	ret.push_back(0x00);
+
+	sofs = ret.size();
+
+	ret += encodeNumberASN1(_msg.id);
+	ret += encodeNumberASN1(_msg.ers);
+	ret += encodeNumberASN1(_msg.eri);
+
+	ret.push_back(0x30);
+	ret.push_back(0x00);
+
+	bofs = ret.size();
+
+	for (const auto& pair : _msg.binds) 
+	{
+		ret.push_back(0x30);
+		ret.push_back(0x00);
+
+		cofs = ret.size();
+
+		ret.push_back(0x06);
+		ret.push_back(pair.first.size());
+
+		ret += pair.first;
+
+		ret.push_back(0x05);
+		ret.push_back(pair.second.size());
+
+		ret += pair.second;
+
+		ret[cofs - 1] = ret.size() - cofs;
+	}
+
+	ret[sofs - 1] = ret.size() - sofs;
+	ret[bofs - 1] = ret.size() - bofs;
+
+	ret.insert(ret.begin(), ret.size());
+	ret.insert(ret.begin(), 0x30);
+
+	return ret;
+}
+
+bool ServerSNMP::sendRequestMessage(std::string _ip, SNMP_SESSION _ses, SNMP_FRAME _msg)
+{
+	return sendSimpleMessage(encodeFrameASN1(_ses, _msg), _ip, 161);
+}
+
 void ServerSNMP::processReceivedMessage(std::string _msg, uint16_t _port)
 {
 }
--- SocketCommunication/source/main.cpp
+++ SocketCommunication/source/main.cpp
@@ -1,14 +1,16 @@
-#include "ServerDHCP.h"
+#include "ServerSNMP.h"
 
 int main()
 {
-	ServerDHCP s("192.168.0.1", "255.255.0.0");
+	ServerSNMP s;
 
-	s.insert("192.168.0.100");
-	s.insert("192.168.0.101");
-	s.insert("192.168.0.102");
+	SNMP_FRAME fr;
 
-	while (1);
+	fr.community = "public";
+	fr.id = 12345;
+	fr.binds.push_back(std::make_pair(std::string{ 0x2B, 0x06, 0x01, 0x02, 0x01 }, ""));
+
+	s.sendRequestMessage("192.168.56.101", SNMP_SESSION::GET_REQUEST, fr);
 
 	return 0;
 }
