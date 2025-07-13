ca879fe399ac927afaf9e940f7f702cdfed890c6
metrovian <paramedoct@gmail.com>
2025-07-13 21:04:15 +0900

    솔루션 명칭 업데이트

--- SocketCommunication/source/main.cpp
+++ SocketCommunication/source/main.cpp
@@ -3,15 +3,5 @@
 
 int main()
 {
-	ServerSNMP s;
-
-	SNMP_FRAME fr;
-
-	fr.community = "public";
-	fr.id = 12345;
-	fr.binds.push_back(std::make_pair(std::string{ 0x2B, 0x06, 0x01, 0x02, 0x01 }, ""));
-
-	s.sendRequestMessage("192.168.50.102", SNMP_SESSION::GET_REQUEST, fr);
-
 	return 0;
 }
